ff9f087595e8e4143df47de45b3a0953319d9fe8
Runatorium <stefano.alesci1@gmail.com>
2023-05-24 16:28:11 +0200

    Migration per foreign key on "posts" table added

---
+++ database/migrations/2023_05_24_142715_add_type_id_to_posts_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('SET NULL');
+
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_type_id_foreign');
+            $table->dropColumn('type_id');
+        });
+    }
+};
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 @section('content')
     <div class="container ">
         <h2 class="fs-4 text-secondary my-4">
